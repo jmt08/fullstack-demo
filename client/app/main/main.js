1cc5014543dbbade26acee7cbfff23300c065370
jmt <jmt@hzjs.co>
2016-11-22 11:01:00 +0800

    init

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('jmtApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
